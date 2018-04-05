7cedc768e6b16644602435d21bf97284ddcd929a
frs123456789 <league@WTS20.attlocal.net>
2018-04-04 19:57:30 -0700

    Secret Message Box and Un Birthday

---
+++ src/UnBirthday.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class UnBirthday {
+
+	
+public static void main(String[] args) {
+	
+	
+	
+	String date = JOptionPane.showInputDialog("What is your birthday? (ex, 12/25)");	
+	
+	if (date.equals("12/29") ) { JOptionPane.showMessageDialog(null, "Happy Birthday!!!");
+	
+	
+	}
+	
+	else { JOptionPane.showMessageDialog(null, "Happy UN-Birthday!!!");
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
